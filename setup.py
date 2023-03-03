5fc29dcf7b5279ef8b40f2717aec9e075687e611
Evgeniy Karasev <karasevevg@gmail.com>
2023-03-03 12:14:07 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chatgpt_bot",
+    version="0.0.1",
+    description="Generic Telegram bot using ChatGPT API",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
